fe061cf6a309d35b5e3834948709c30ea1fde042
16sheep <marjutubli@gmail.com>
2017-05-31 20:53:40 +0100

    Fix trailer

--- movie_website/fresh_tomatoes.py
+++ movie_website/fresh_tomatoes.py
@@ -131,8 +131,8 @@ main_page_content = '''
 
 # A single movie entry html template
 movie_tile_content = '''
-<div class="col-md-6 col-lg-4 movie-tile text-center">
-    <img src="{poster_image_url}" width="220" height="342" data-trailer-youtube-id="{trailer_youtube_id}" data-toggle="modal" data-target="#trailer">
+<div class="col-md-6 col-lg-4 movie-tile text-center" data-trailer-youtube-id="{trailer_youtube_id}" data-toggle="modal" data-target="#trailer">
+    <img src="{poster_image_url}" width="220" height="342">
     <a href="{imdb_link}" target="_blank"><h4>{movie_title}</h4></a>
 </div>
 '''
